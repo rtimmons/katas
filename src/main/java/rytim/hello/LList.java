18818cb0e2cf126abf9cab432fc7b23b444e8882
Ryan Timmons <ryan@rytim.com>
2017-08-11 08:23:57 -0400

    LList cycles

---
+++ src/main/java/rytim/hello/LList.java
@@ -0,0 +1,47 @@
+package rytim.hello;
+
+public class LList<T> {
+
+    final T data;
+    LList<T> next;
+
+    public LList(T data) {
+        this.data = data;
+    }
+
+    boolean cycles() {
+        return cycles(this);
+    }
+
+    static boolean cycles(LList<?> n) {
+        LList slow = n;
+        LList fast = n;
+
+        while(slow != null) {
+            slow = slow.next;
+
+            if ( fast.next != null ) {
+                fast = fast.next;
+            }
+            if ( fast != null && fast.next != null ) {
+                fast = fast.next;
+                if ( fast == slow ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String...arg) {
+        LList<Integer> h1 = new LList<>(1);
+        LList<Integer> h2 = new LList<>(2);
+        LList<Integer> h3 = new LList<>(3);
+        h3.next = h1;
+
+        h1.next = h2;
+        h2.next = h3;
+
+        System.out.println(h1.cycles());
+    }
+}
