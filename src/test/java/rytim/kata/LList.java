6f455d6f53ab528dead34e7f10aca2083f00365d
Ryan Timmons <rtimmons@betterment.com>
2017-08-17 16:27:01 -0400

    Delegate

--- src/test/java/rytim/kata/LList.java
+++ src/test/java/rytim/kata/LList.java
@@ -2,49 +2,62 @@ package rytim.kata;
 
 public class LList<T> {
 
-    final T data;
-    LList<T> next;
+
+    Delegate<T> delegate;
 
     public LList(T data) {
-        this.data = data;
+        this.delegate = new Delegate<>(data);
     }
 
-    boolean cycles() {
-        return cycles(this);
-    }
+    public static class Delegate<T> {
 
-    static boolean cycles(LList<?> n) {
-        LList slow = n;
-        LList fast = n;
+        final T data;
+        Delegate<T> next;
 
-        while(slow != null) {
-            slow = slow.next;
-
-            if ( fast.next != null ) {
-                fast = fast.next;
-                if (fast == slow) {
-                    return true;
-                }
-            }
-            if ( fast.next != null ) {
-                fast = fast.next;
-                if ( fast == slow ) {
-                    return true;
-                }
-            }
+        public Delegate(T data) {
+            this.data = data;
         }
-        return false;
+
+        boolean cycles() {
+            return cycles(this);
+        }
+
+        static boolean cycles(Delegate<?> n) {
+            Delegate slow = n;
+            Delegate fast = n;
+
+            while (slow != null) {
+                slow = slow.next;
+
+                if (fast.next != null) {
+                    fast = fast.next;
+                    if (fast == slow) {
+                        return true;
+                    }
+                }
+                if (fast.next != null) {
+                    fast = fast.next;
+                    if (fast == slow) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 
-    public static void main(String...arg) {
+    // TODO: don't expose the delegate! dirty refactor to support #remove()
+    public static void main(String... arg) {
         LList<Integer> h1 = new LList<>(1);
         LList<Integer> h2 = new LList<>(2);
         LList<Integer> h3 = new LList<>(3);
-        h3.next = h1;
+        h3.delegate.next = h1.delegate;
 
-        h1.next = h2;
-        h2.next = h3;
+        h1.delegate.next = h2.delegate;
+        h2.delegate.next = h3.delegate;
 
-        System.out.println(h1.cycles());
+        System.out.println(h1.delegate.cycles());
     }
 }
+
+
