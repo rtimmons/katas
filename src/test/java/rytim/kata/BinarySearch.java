a7b01221d1e79ce32bca60aaf0cbb03a10ab02a8
Ryan Timmons <ryan@rytim.com>
2017-08-26 11:58:11 -0400

    bugfix

--- src/test/java/rytim/kata/BinarySearch.java
+++ src/test/java/rytim/kata/BinarySearch.java
@@ -10,28 +10,35 @@ import org.junit.Test;
 public class BinarySearch {
     static int indexOf(int needle, int[] haystack) {
         int low = 0;
-        int hi  = haystack.length - 1;
-        while(low < hi) {
-            int mid = (hi - low) / 2;
-            if(haystack[mid] < needle) {
-                hi = mid + 1;
+        int high = haystack.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long midVal = haystack[mid];
+
+            if (midVal < needle) {
+                low = mid + 1;
             }
-            else if (haystack[mid] > needle) {
-                low = mid - 1;
+            else if (midVal > needle) {
+                high = mid - 1;
             }
             else {
                 return mid;
             }
         }
-        return -(low - 1);
+        return -1;
     }
 
     public static class Tests {
         @Test
         public void tests() {
-            Assert.assertEquals(0, indexOf(1, new int[]{ 1 }));
-            Assert.assertEquals(1, indexOf(1, new int[]{ 0, 1 }));
-            Assert.assertEquals(0, indexOf(0, new int[]{ 0, 1 }));
+            Assert.assertEquals("Single element", 0, indexOf(1, new int[]{ 1 }));
+            Assert.assertEquals("last", 1, indexOf(1, new int[]{ 0, 1 }));
+            Assert.assertEquals("first", 0, indexOf(0, new int[]{ 0, 1 }));
+            Assert.assertEquals("middle", 1, indexOf(1, new int[]{ 0, 1, 2 }));
+            Assert.assertEquals("last3", 2, indexOf(2, new int[]{ 0, 1, 2 }));
+            Assert.assertEquals("lastrev", 2, indexOf(1, new int[]{ -1, 0, 1 }));
+            Assert.assertEquals("notfound", -1, indexOf(1, new int[]{ 2,2,2 }));
         }
     }
 }
